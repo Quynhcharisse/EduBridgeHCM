9b75f2b6d763f9aabd184554ae5f69d22d518559
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2026-02-27 13:49:47 +0700

    feat: setup root directory structure on feature/admin

---
+++ src/main/java/com/sp26se041/edubridgehcm/EduBridgeHcmApplication.java
@@ -0,0 +1,13 @@
+package com.sp26se041.edubridgehcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduBridgeHcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduBridgeHcmApplication.class, args);
+    }
+
+}
