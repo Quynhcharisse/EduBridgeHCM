c49cda2229f17bc8c4f09156d4501db1b2367319
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2026-02-27 13:44:40 +0700

    first commit

---
+++ src/test/java/com/sp26se041/edubridgehcm/EduBridgeHcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp26se041.edubridgehcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduBridgeHcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
