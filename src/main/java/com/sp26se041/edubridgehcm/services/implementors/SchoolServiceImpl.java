651823af152689f86898542101fa232b118658a0
quynhKute <vannhuquynhp@gmail.com>
2026-04-01 20:13:55 +0700

    fix: fix status for curriculum

--- src/main/java/com/sp26se041/edubridgehcm/services/implementors/SchoolServiceImpl.java
+++ src/main/java/com/sp26se041/edubridgehcm/services/implementors/SchoolServiceImpl.java
@@ -701,8 +701,7 @@ public class SchoolServiceImpl implements SchoolService {
         data.put("curriculumType", curriculum.getCurriculumType());
         data.put("methodLearning", curriculum.getMethodLearning());
         data.put("enrollmentYear", curriculum.getEnrollmentYear());
-        data.put("groupCode", curriculum.getGroupCode());
-        data.put("curriculumStatus", curriculum.getCurriculumStatus().name());
+        data.put("groupCode", curriculum.getGroupCode());t
         data.put("subjects", curriculum.getSubjectsJsonb());
         data.put("status", curriculum.getCurriculumStatus().name());
 
