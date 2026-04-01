af9628bd851110de5058e4910151842a4cc376b0
quynhKute <vannhuquynhp@gmail.com>
2026-04-01 14:51:07 +0700

    feat: add new cloneAdmissionCampaign

--- src/main/java/com/sp26se041/edubridgehcm/repositories/AdmissionReservationFormRepo.java
+++ src/main/java/com/sp26se041/edubridgehcm/repositories/AdmissionReservationFormRepo.java
@@ -4,6 +4,9 @@ import com.sp26se041.edubridgehcm.models.AdmissionReservationForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdmissionReservationFormRepo extends JpaRepository<AdmissionReservationForm, Integer> {
+
     int countByCampusProgramOfferingId(Integer campusProgramOfferingId);
+
+    int countByCampusProgramOffering_AdmissionCampaign_Id(int id);
 }
 
