20f17c08778dfe950f3ee82c6f355f586e89e0b9
quynhKute <vannhuquynhp@gmail.com>
2026-04-01 20:05:29 +0700

    fix: fix validation for upset curriculum

--- src/main/java/com/sp26se041/edubridgehcm/repositories/CurriculumRepo.java
+++ src/main/java/com/sp26se041/edubridgehcm/repositories/CurriculumRepo.java
@@ -6,11 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CurriculumRepo extends JpaRepository<Curriculum, Integer> {
 
     Page<Curriculum> findBySchoolIdOrderByEnrollmentYearDesc(Integer schoolId, Pageable pageable);
 
     Curriculum findByGroupCodeAndEnrollmentYearAndCurriculumStatus(String groupCode, int enrollmentYear, Status status);
+
+    boolean existsByGroupCodeAndEnrollmentYearAndCurriculumStatusNotAndIdNot(String groupCode, int enrollmentYear, Status status, int curriculumId);
 }
