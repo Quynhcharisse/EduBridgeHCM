9bf30fd9d161f2148a0c43a403e27f6f4247cd8e
quynhKute <vannhuquynhp@gmail.com>
2026-04-10 20:15:25 +0700

    fix: fix code update validation

--- src/main/java/com/sp26se041/edubridgehcm/requests/UpdateProfileRequest.java
+++ src/main/java/com/sp26se041/edubridgehcm/requests/UpdateProfileRequest.java
@@ -7,8 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -97,10 +95,6 @@ public class UpdateProfileRequest {
 
         String websiteUrl;
 
-        String representativeName;
-
         String hotline;
-
-        LocalDate foundingDate;
     }
 }
