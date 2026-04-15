2851d2d02696823e0b17362011a09431d59569e1
quynhKute <vannhuquynhp@gmail.com>
2026-04-15 18:48:08 +0700

    fix: fix content display

--- src/main/java/com/sp26se041/edubridgehcm/requests/CreateHolidayRequest.java
+++ src/main/java/com/sp26se041/edubridgehcm/requests/CreateHolidayRequest.java
@@ -7,10 +7,33 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreateHolidayRequest {
+
+    String title;
+
+    LocalDate startDate;
+
+    LocalDate endDate;
+
+    // true: Áp dụng toàn trường (campus_id sẽ null)
+    // false: Chỉ áp dụng cho 1 campus cụ thể
+    Boolean isGlobal;
+
+    // ID của campus nhận lịch nghỉ (Bắt buộc nếu isGlobal = false)
+    Integer campusId;
+
+    // true: Tư vấn viên nghỉ (ẩn slot)
+    // false: Chỉ học sinh nghỉ (vẫn hiện slot)
+    Boolean applyToConsultant;
+
+    // Mặc định false. Nếu Backend báo có lịch đã đặt (Conflict),
+    // Frontend sẽ gửi lại request với forceCreate = true để xác nhận hủy.
+    Boolean forceCreate; //==> Cờ xác nhận (Xử lý Level 2)
 }
