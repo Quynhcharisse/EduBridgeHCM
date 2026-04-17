f448fb2611fea5a8ca6c1660ab92244dbd814cfa
quynhKute <vannhuquynhp@gmail.com>
2026-04-17 15:14:18 +0700

    fix: fix value enum not necessary

--- src/main/java/com/sp26se041/edubridgehcm/enums/ParentPostPermission.java
+++ src/main/java/com/sp26se041/edubridgehcm/enums/ParentPostPermission.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ParentPostPermission {
-    NONE("none"), // Không có quyền gì (thậm chí không thấy mục cộng đồng)
     VIEW_ONLY("view only"), // Chỉ được xem bài viết của trường
     CREATE_POST("create post"); // Được phép đăng bài mới
     private final String value;
