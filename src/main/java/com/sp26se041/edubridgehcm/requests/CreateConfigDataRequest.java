327aeb937f3300adf606c33de2caf11fd5d42694
quynhKute <vannhuquynhp@gmail.com>
2026-04-22 12:48:06 +0700

    fix: fix cors for get config system by key

--- src/main/java/com/sp26se041/edubridgehcm/requests/CreateConfigDataRequest.java
+++ src/main/java/com/sp26se041/edubridgehcm/requests/CreateConfigDataRequest.java
@@ -1,6 +1,10 @@
 package com.sp26se041.edubridgehcm.requests;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.util.List;
@@ -58,6 +62,7 @@ public class CreateConfigDataRequest {
     @FieldDefaults(level = AccessLevel.PRIVATE)
     public static class FeatureUnitPrices {
         double extraCounsellorSlot;
+        double extraPostFee;
         double aiChatbotMonthlyFee;
         double premiumSupportFee;
         double topRankingFee;
