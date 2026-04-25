8dce7a7e8f58045206166c9ba16f3c04cca09bc3
quynhKute <vannhuquynhp@gmail.com>
2026-04-25 18:58:19 +0700

    fix: fix offering request

--- src/main/java/com/sp26se041/edubridgehcm/requests/CreateCampusProgramOfferingRequest.java
+++ src/main/java/com/sp26se041/edubridgehcm/requests/CreateCampusProgramOfferingRequest.java
@@ -4,9 +4,6 @@ import com.sp26se041.edubridgehcm.enums.LearningMode;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,12 +19,6 @@ public class CreateCampusProgramOfferingRequest {
 
     LearningMode learningMode;
 
-    BigDecimal tuitionFee;
-
     Float priceAdjustmentPercentage;
-
-    LocalDate openDate;
-
-    LocalDate closeDate;
 }
 
