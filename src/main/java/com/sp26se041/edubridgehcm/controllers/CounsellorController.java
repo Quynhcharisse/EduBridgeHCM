856ce547e8cf27dd9600914460110ed4c6cc9674
TriTVH <161578251+TriTVH@users.noreply.github.com>
2026-05-06 12:29:53 +0700

    Update import

--- src/main/java/com/sp26se041/edubridgehcm/controllers/CounsellorController.java
+++ src/main/java/com/sp26se041/edubridgehcm/controllers/CounsellorController.java
@@ -11,7 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
