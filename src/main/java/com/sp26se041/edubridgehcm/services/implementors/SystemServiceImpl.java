83ca3242513d7a3e8dd406e31b5724fb315a8b5a
quynhKute <vannhuquynhp@gmail.com>
2026-05-11 14:50:03 +0700

    fix: fix response body

--- src/main/java/com/sp26se041/edubridgehcm/services/implementors/SystemServiceImpl.java
+++ src/main/java/com/sp26se041/edubridgehcm/services/implementors/SystemServiceImpl.java
@@ -520,7 +520,6 @@ public class SystemServiceImpl implements SystemService {
         Map<String, Object> admissionJson = new HashMap<>();
         admissionJson.put("allowedMethods", allowedMethodsJson);
         admissionJson.put("admissionProcesses", admissionProcessesJson);
-        admissionJson.put("byMethod", byMethodJson);
         admissionJson.put("documentRequirementsData", documentRequirementsTemplateJson);
 
         PlatformConfig config = platformConfigRepo.findByKey("admissionSettingsData").orElse(
