baef205d8aa65ddc5b0837f0bc914f9b0c7f1e09
TriTVH <161578251+TriTVH@users.noreply.github.com>
2026-05-24 12:00:53 +0700

    fix: fix update student info

--- src/main/java/com/sp26se041/edubridgehcm/services/implementors/ParentServiceImpl.java
+++ src/main/java/com/sp26se041/edubridgehcm/services/implementors/ParentServiceImpl.java
@@ -1048,15 +1048,20 @@ public class ParentServiceImpl implements ParentService {
             return "Giới tính không hợp lệ";
         }
 
-        Optional<PersonalityType> personalityType =
-                personalityTypeRepo.findByCode(request.getPersonalityTypeCode());
-        if (personalityType.isEmpty()) {
-            return "Loại tính cách không hợp lệ";
+        if (request.getPersonalityTypeCode() != null || !request.getPersonalityTypeCode().trim().isBlank()) {
+
+            Optional<PersonalityType> personalityType =
+                    personalityTypeRepo.findByCode(request.getPersonalityTypeCode());
+            if (personalityType.isEmpty()) {
+                return "Loại tính cách không hợp lệ";
+            }
         }
 
-        Optional<Major> major = majorRepo.findByName(request.getFavouriteJob());
-        if (major.isEmpty()) {
-            return "Ngành nghề yêu thích không hợp lệ";
+        if (request.getFavouriteJob() != null || !request.getFavouriteJob().trim().isBlank()) {
+            Optional<Major> major = majorRepo.findByName(request.getFavouriteJob());
+            if (major.isEmpty()) {
+                return "Ngành nghề yêu thích không hợp lệ";
+            }
         }
 
         if (request.getAcademicInfos() == null || request.getAcademicInfos().isEmpty()) {
